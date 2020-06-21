55303e2ee5cf07ad7d1a68a9dd2eef823564b287
Mattias Törnqvist <mattias.trnqvist@gmail.com>
2020-06-21 20:46:57 +0200

    Initial Commit

---
+++ Activation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Activation
+//
+//  Created by Mattias Törnqvist on 2020-06-21.
+//  Copyright © 2020 Mattias Törnqvist. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
